60782882faf0e9805271db53e33f916fd3d9f59f
Joe Fiorini <joe@joefiorini.com>
2014-02-26 20:21:58 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SurveyGorilla::Application.config.secret_key_base = '1e1abf73953fc17338fdabd968f5d283447ce7f73dcd66eb72fbbd2ae60ec68a74ea8cbb889510964839fbc846af4d0ac4f494d836971e1dd5ee2db4ac7f8f54'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SurveyGorilla::Application.config.session_store :cookie_store, key: '_survey_gorilla_session'
